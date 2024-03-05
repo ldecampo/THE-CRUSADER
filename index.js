d40bd756900a596fe35b6fe96bb6a1fcb639ea47
Kenorbs <80411201+TheMostOGName@users.noreply.github.com>
2024-03-05 16:52:54 -0500

    Added timeout functionality

--- index.js
+++ index.js
@@ -19,8 +19,13 @@ client.once(Events.ClientReady, readyClient => {
 
 client.on("messageCreate", (message) => {
 
+    
+
     //Check for test words
     for (let word of unsafeWords.test) {
+        if (message.author == 1095366459191984198) {
+            break
+        }
         if (message.content.toLowerCase().includes(word)) {
             message.reply('Successful test.');
             console.log("Message heard.");
@@ -29,23 +34,59 @@ client.on("messageCreate", (message) => {
 
     //Check for slurs words
     for (let word of unsafeWords.slurs) {
+        if (message.author == 1095366459191984198) {
+            break
+        }
         if (message.content.toLowerCase().includes(word)) {
             message.reply('<a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167> SLUR DETECTED <a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167> \n <@&1095738454157041725> ENGAGE \n TIMING OUT MEMBER FOR 10 MINUTES');
-            // message.member.timeout(10 * 60 * 1000, "Slur Detected.")
+                        //Attempt timeout
+                        message.guild.members.fetch(message.author)
+                        .then(member => {
+                            // Use the guild member timeout method
+                            member.timeout(600000,'Slur Detected')
+                                .then(() => {
+                                    console.log('Timed user out for 10 minutes.');
+                                    client.channels.fetch(secrets.logID).then(channel => {
+                                    channel.send(">>> " + "Timed out user <@" + message.author + "> for ten minutes due to slur at " + message.url + "\n Message content: \"" + message.content + "\"");
+                                    })
+                                })
+                                .catch(console.error);
+                        })
+                        .catch(console.error);
         }
     }
 
     //Check for timeouttest words
     for (let word of unsafeWords.timeOutTest) {
+        if (message.author == 1095366459191984198) {
+            break
+        }
         if (message.content.toLowerCase().includes(word)) {
             message.reply('Successful test, attempting timeout...')
-            // message.member.timeout(10 * 60, "Timeout Test.")
+            //Attempt timeout
+            message.guild.members.fetch(message.author)
+            .then(member => {
+                // Use the guild member timeout method
+                member.timeout(600000,'Slur Detected')
+                    .then(() => {
+                        console.log('Timed user out for 10 minutes.');
+                        client.channels.fetch(secrets.logID).then(channel => {
+                        channel.send(">>> " + "Timed out user <@" + message.author + "> for ten minutes due to test at " + message.url + "\n Message content: \"" + message.content + "\"");
+                        })
+                    })
+                    .catch(console.error);
+            })
+            .catch(console.error);
+    
         }
 
     }
 
     //Check for french words
     for (let word of unsafeWords.french) {
+        if (message.author == 1095366459191984198) {
+            break
+        }
         if (message.content.toLowerCase().includes(word)) {
             let rand = Math.floor(Math.random() * 10);
             if (rand == 3) { 
@@ -53,6 +94,28 @@ client.on("messageCreate", (message) => {
           }
         }
     }
+
+    // if (unsafeWords.slurs.some(word => message.content.toLowerCase().includes(" " + word + " "))) {
+    //   message.reply('<a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167> SLUR DETECTED <a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167> \n <@&1095738454157041725> ENGAGE \n TIMING OUT MEMBER FOR 10 MINUTES');
+    //       //message.member.timeout(10 * 60 * 1000, "Slur Detected.")
+    // }
+    // if (unsafeWords.timeOutTest.some(word => message.content.toLowerCase().includes(" " + word + " "))) {
+    //       message.reply('Successful test, attempting timeout...')
+    //       //message.member.timeout(10 * 60, "Timeout Test.")
+    // }
+    // if (unsafeWords.slurs.some(word => message.content.toLowerCase().includes(word))) {
+    //   message.reply('<a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167> POSSIBLE SLUR DETECTED <a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167> \n <@&1095738454157041725> ENGAGE');
+    // }
+    // if (unsafeWords.test.some(word => message.content.toLowerCase().includes(" " + word + " "))) {
+    //   message.reply('Successful test.');
+    //   console.log("Message heard.")
+    // }
+    // if (unsafeWords.french.some(word => message.content.toLowerCase().includes(" " + word + " "))) {
+    //       let rand = Math.floor(Math.random() * 10);
+    //       if (rand == 3) { 
+    //       message.reply('<a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167> SLUR DETECTED <a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167><a:alert:1095711502683611167> \n KNIGHTS ENGAGE');
+    //       }
+    // }
 });
 
   
