0b04edd05dc5363ad35763bb97cded2d00b1c839
Kenorbs <80411201+TheMostOGName@users.noreply.github.com>
2024-03-18 11:52:35 -0400

    Added four commands
    Register.js creates a unique json file for a server in the guilds folder. 
    setLogChannel.js sets the current channel as the log channel for the current guild
    setPinLogChannel sets the current channel as the pin log channel for the current guild
    ping returns pong! (testing/upkeep command)

---
+++ commands/utility/register.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('register')
+		.setDescription('Registers the server to the bot')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+	async execute(interaction) {
+        const filePath = './guilds/' + interaction.guild.id + '.json';
+
+        fs.access(filePath, fs.constants.F_OK, (err) => {
+            if (err) {
+                var examplePins = [1, 2, 3]
+                var jsonArray = { 
+                    "guildId"  :  interaction.guild.id, 
+                    "logID"   :  "", 
+                    "pinID"      :  "",
+                    "pins"      : examplePins,
+                    }
+                
+                require("fs").writeFileSync(filePath, JSON.stringify(jsonArray));
+                interaction.reply("Server registered")
+
+            } else {
+                interaction.reply("Error: server already registered")
+            }
+          });
+
+	},
+};
---
+++ commands/utility/setLogChannel.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('setlogchannel')
+		.setDescription('Registers the current channel as the log to the bot')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
+	async execute(interaction) {
+        try {   
+            const guildId = interaction.guild.id;
+            const filePath = path.resolve(__dirname, `../../guilds/${guildId}.json`);
+            const channelID = interaction.channelId
+            console.log(channelID)
+
+            // Check if channelID is valid
+            if (!channelID) {
+                return interaction.reply("Invalid channel ID");
+            }
+
+            // Read JSON file
+            let serverInfo = require(filePath); 
+            serverInfo.logID = channelID;
+
+            
+            // Write JSON data back to file
+            fs.writeFileSync(filePath, JSON.stringify(serverInfo, null, 2)); // Pretty print JSON
+
+            // Reply to interaction
+            interaction.reply("Channel set");
+        } catch (Exception) {
+            interaction.reply("Error setting channel")
+            console.log(Exception)
+        }
+          
+
+	},
+};
